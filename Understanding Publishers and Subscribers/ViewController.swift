48067a812f56397fd50968f099e2800fa2dda3d6
Mphathi Maapola <113903905+Patglu@users.noreply.github.com>
2023-03-17 19:28:34 +0200

    basic observer and notification set up

--- Understanding Publishers and Subscribers/ViewController.swift
+++ Understanding Publishers and Subscribers/ViewController.swift
@@ -11,7 +11,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let notification = Notification.Name("MyNotification")
+        let center = NotificationCenter.default
+        
+        /// set up the observer
+        let observer = center.addObserver(forName: notification, object: nil, queue: nil) { notification in
+            print("Notification recieved")
+        }
+        
+        center.post(name: notification, object: nil)
+        /// The observer will be fired off because it's looking for a notification ("MyNotification")
+        /// once you call a notificaiton you want to remove it once you're done
+        
+        center.removeObserver(observer)
+        
     }
 
 
