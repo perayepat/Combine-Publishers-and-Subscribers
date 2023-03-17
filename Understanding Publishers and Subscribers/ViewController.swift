d2d9298dc01d3b338a4ba17dd074d89eac299f65
Mphathi Maapola <113903905+Patglu@users.noreply.github.com>
2023-03-17 20:23:50 +0200

    Implementing subjects

--- Understanding Publishers and Subscribers/ViewController.swift
+++ Understanding Publishers and Subscribers/ViewController.swift
@@ -2,6 +2,62 @@
 import UIKit
 import Combine
 
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            
+  
+
+    }
+}
+
+//MARK: -  Using the notificaiton center
+extension ViewController{
+    func usingImperativeProgramming(){
+        //MARK: - Imperative programming
+        let notification = Notification.Name("MyNotification")
+        let center = NotificationCenter.default
+        
+        /// set up the observer
+        let observer = center.addObserver(forName: notification, object: nil, queue: nil) { notification in
+            print("Notification recieved")
+        }
+        
+        center.post(name: notification, object: nil)
+        /// The observer will be fired off because it's looking for a notification ("MyNotification")
+        /// once you call a notificaiton you want to remove it once you're done
+        
+        center.removeObserver(observer)
+    }
+}
+
+
+//MARK: - Sending notificaitons using publisher and subscriber
+extension ViewController{
+    func usingTheObserverPublihserPattern(){
+        let notification = Notification.Name("MyPublisherNotification")
+        
+        let publisher = NotificationCenter.default.publisher(for: notification,object: nil)
+        
+        ///The `sink` function means that you want to subscribe to that specific publisher
+        ///this does return a sub
+        ///Needs to be before the post other wise nothing gets fired off
+        let subscription = publisher.sink { _ in
+            print("Notification recieved from sink")
+        }
+        
+        NotificationCenter.default.post(name: notification, object: nil)
+        
+        ///This is how we cancel a subscription so that we won't get any updates
+        subscription.cancel()
+    
+    }
+}
+
+//MARK: - Implementing a subscriber
+
 class StringSubscriber: Subscriber{
     
     //Gets the sub
@@ -28,55 +84,12 @@ class StringSubscriber: Subscriber{
     typealias Failure = Never
 }
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-            
+extension ViewController{
+    func subscriberClass(){
         let publisher = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"].publisher
         
         let subscriber = StringSubscriber()
         publisher.subscribe(subscriber)
         
-
     }
 }
-
-extension ViewController{
-    func usingImperativeProgramming(){
-        //MARK: - Imperative programming
-        let notification = Notification.Name("MyNotification")
-        let center = NotificationCenter.default
-        
-        /// set up the observer
-        let observer = center.addObserver(forName: notification, object: nil, queue: nil) { notification in
-            print("Notification recieved")
-        }
-        
-        center.post(name: notification, object: nil)
-        /// The observer will be fired off because it's looking for a notification ("MyNotification")
-        /// once you call a notificaiton you want to remove it once you're done
-        
-        center.removeObserver(observer)
-    }
-    
-    func usingTheObserverPublihserPattern(){
-        let notification = Notification.Name("MyPublisherNotification")
-        
-        let publisher = NotificationCenter.default.publisher(for: notification,object: nil)
-        
-        ///The `sink` function means that you want to subscribe to that specific publisher
-        ///this does return a sub
-        ///Needs to be before the post other wise nothing gets fired off
-        let subscription = publisher.sink { _ in
-            print("Notification recieved from sink")
-        }
-        
-        NotificationCenter.default.post(name: notification, object: nil)
-        
-        ///This is how we cancel a subscription so that we won't get any updates
-        subscription.cancel()
-    
-    }
-}
-
